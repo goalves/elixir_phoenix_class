47339e306660a13c524a2ff9ca842e91002aed14
Gabriel Alves <g04lves@gmail.com>
2019-06-14 16:05:46 -0300

    Enhances user controller to have all actions.

--- lib/elixir_phoenix_class_web/controllers/user_controller.ex
+++ lib/elixir_phoenix_class_web/controllers/user_controller.ex
@@ -5,10 +5,44 @@ defmodule EPClassWeb.UserController do
   action_fallback(EPClassWeb.DefaultFallbackController)
 
   def create(conn, params) do
-    {:ok, user} = Accounts.create_user(params)
+    with {:ok, user} <- Accounts.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("show.json", %{user: user})
+    end
+  end
+
+  def update(conn, params = %{"id" => id}) do
+    with {:ok, user} <- Accounts.get_user(id),
+         {:ok, updated_user} <- Accounts.update_user(user, params) do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", %{user: updated_user})
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    with {:ok, user} <- Accounts.get_user(id),
+         {:ok, _} <- Accounts.delete_user(user) do
+      conn
+      |> put_status(:no_content)
+      |> json("")
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, user} <- Accounts.get_user(id) do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", %{user: user})
+    end
+  end
+
+  def index(conn, _) do
+    users = Accounts.list_users()
 
     conn
     |> put_status(:ok)
-    |> render("user.json", %{user: user})
+    |> render("index.json", %{users: users})
   end
 end
