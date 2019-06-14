32055edf1cc6c65a51f9b7dfb31b3b22a59b68f9
Gabriel Alves <g04lves@gmail.com>
2019-06-14 15:01:49 -0300

    Adds functions to actually make tests pass! :)

--- lib/elixir_phoenix_class/accounts/accounts.ex
+++ lib/elixir_phoenix_class/accounts/accounts.ex
@@ -7,4 +7,27 @@ defmodule EPClass.Accounts do
     |> User.changeset(changes)
     |> Repo.insert()
   end
+
+  def update_user(user, changes) do
+    user
+    |> User.changeset(changes)
+    |> Repo.update()
+  end
+
+  def delete_user(user) do
+    Repo.delete(user)
+  end
+
+  def list_users() do
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    User
+    |> Repo.get(id)
+    |> case do
+      nil -> {:error, :user_does_not_exist}
+      user -> {:ok, user}
+    end
+  end
 end
