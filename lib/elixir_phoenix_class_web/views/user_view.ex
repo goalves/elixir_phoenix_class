c6b18d5f1fa7828504235540f660eecf19ce72bf
Gabriel Alves <g04lves@gmail.com>
2019-06-14 16:06:00 -0300

    Enhances user view to be able to render multiple elements.

--- lib/elixir_phoenix_class_web/views/user_view.ex
+++ lib/elixir_phoenix_class_web/views/user_view.ex
@@ -1,6 +1,12 @@
 defmodule EPClassWeb.UserView do
   use EPClassWeb, :view
 
+  def render("index.json", %{users: users}),
+    do: render_many(users, __MODULE__, "user.json")
+
+  def render("show.json", %{user: user}),
+    do: render_one(user, __MODULE__, "user.json")
+
   def render("user.json", %{user: user}) do
     %{
       id: user.id,
