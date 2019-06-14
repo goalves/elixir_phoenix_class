1c152411f64ad14c05b87f640444ed012b23531c
Gabriel Alves <g04lves@gmail.com>
2019-06-14 15:28:52 -0300

    Calls view on user controller.

--- lib/elixir_phoenix_class_web/controllers/user_controller.ex
+++ lib/elixir_phoenix_class_web/controllers/user_controller.ex
@@ -3,7 +3,10 @@ defmodule EPClassWeb.UserController do
   alias EPClass.Accounts
 
   def create(conn, params) do
-    {:ok, user} = Accounts.create_user(params) |> IO.inspect()
-    send_resp(conn, :ok, "created the user")
+    {:ok, user} = Accounts.create_user(params)
+
+    conn
+    |> put_status(:ok)
+    |> render("user.json", %{user: user})
   end
 end
